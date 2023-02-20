a09c0161da6c1d61f92d4a21a2e8d170535948a0
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-02-20 02:12:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def find_mismatch(text):
         
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_sack or not
+            if not opening_brackets_stack or not
             are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
