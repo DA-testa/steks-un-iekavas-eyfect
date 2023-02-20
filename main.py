eb67b86c5f06baa8dc5c01f79bd4a99b24ee2bd4
Vasīlijs Dvils-Dmitrijevs <68693904+eyfect@users.noreply.github.com>
2023-02-20 02:16:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i))
+            opening_brackets_stack.append(Bracket(next,i))
             pass
 
         
         
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not
-            are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
                 return i+1
             opening_brackets_stack.pop()
             pass
